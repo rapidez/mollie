c5b9eb0143ddfb5178042ab92c122aa4b7a0f6db
Roy Duineveld <royduin@users.noreply.github.com>
2021-08-03 11:38:39 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
-use Rapidez\Core\Models\Category;
-use Rapidez\Core\Models\Page;
-use Rapidez\Core\Models\Product;
-use Rapidez\Core\Models\Rewrite;
-
 Route::middleware('web')->group(function () {
     Route::get('mollie-return/{orderHash}', function ($orderHash) {
         $url = config('rapidez.magento_url').'/rest/'.config('rapidez.store_code').'/V1/mollie/get-order/'.$orderHash;
-        return view('mollie::' . Http::get($url)->throw()[0]['status']);
+
+        return view('mollie::'.Http::get($url)->throw()[0]['status']);
     });
 });
