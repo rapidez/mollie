664a9c2f9adfd31b885a492502757a91dd4d1fb3
indy koning <indy@justbetter.nl>
2022-02-21 14:27:51 +0100

    Redirect canceled payments to cart, periodically check payment status for pending payment

--- resources/views/pending_payment.blade.php
+++ resources/views/pending_payment.blade.php
@@ -2,11 +2,11 @@
 
 @section('title', 'Pending payment')
 
+@php(header("Refresh:2"))
+
 @section('content')
-    <checkout-success>
-        <div>
-            <h1 class="font-bold text-4xl mb-5">@lang('Pending payment')</h1>
-            <p>@lang('We did not receive the payment (yet).')</p>
-        </div>
-    </checkout-success>
+    <div>
+        <h1 class="font-bold text-4xl mb-5">@lang('Pending payment')</h1>
+        <p>@lang('We did not receive the payment (yet). We will be refreshing the page until we do.')</p>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 Route::middleware('web')->group(function () {
     Route::get('mollie-return/{orderHash}', function ($orderHash) {
         $url = config('rapidez.magento_url').'/rest/'.config('rapidez.store_code').'/V1/mollie/get-order/'.$orderHash;
+        $status = Http::get($url)->throw()[0]['status'];
 
-        return view('mollie::'.Http::get($url)->throw()[0]['status']);
+        if ($status === 'canceled') {
+            return redirect('cart');
+        }
+
+        return view('mollie::'.$status);
     });
 });
