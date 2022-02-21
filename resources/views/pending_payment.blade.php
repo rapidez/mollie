b89f3d6d60bbaa7ff2bf7206dab4e39b825d2355
indy koning <indy@justbetter.nl>
2022-02-21 16:44:57 +0100

    Add extra message to contact if pending payment stays

--- resources/views/pending_payment.blade.php
+++ resources/views/pending_payment.blade.php
@@ -8,5 +8,6 @@
     <div>
         <h1 class="font-bold text-4xl mb-5">@lang('Pending payment')</h1>
         <p>@lang('We did not receive the payment (yet). We will be refreshing the page until we do.')</p>
+        <p>@lang('Are you still seeing this page after a minute? Please contact us.')</p>
     </div>
 @endsection
