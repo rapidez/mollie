4132e3fcf93613dae8b3a0b237026fde74308da8
indykoning <15870933+indykoning@users.noreply.github.com>
2022-02-21 17:12:32 +0100

    Redirect canceled payments to cart, periodically check payment status for pending payment (#3)

--- resources/views/canceled.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('rapidez::layouts.app')
-
-@section('title', 'Order canceled')
-
-@section('content')
-    <h1 class="font-bold text-4xl mb-5">@lang('Order canceled')</h1>
-    <p>@lang('You did not finish the payment so the order is canceled.')</p>
-@endsection
--- resources/views/pending_payment.blade.php
+++ resources/views/pending_payment.blade.php
@@ -2,11 +2,12 @@
 
 @section('title', 'Pending payment')
 
+@php(header("Refresh:2"))
+
 @section('content')
-    <checkout-success>
-        <div>
-            <h1 class="font-bold text-4xl mb-5">@lang('Pending payment')</h1>
-            <p>@lang('We did not receive the payment (yet).')</p>
-        </div>
-    </checkout-success>
+    <div>
+        <h1 class="font-bold text-4xl mb-5">@lang('Pending payment')</h1>
+        <p>@lang('We did not receive the payment (yet). We will be refreshing the page until we do.')</p>
+        <p>@lang('Are you still seeing this page after a minute? Please contact us.')</p>
+    </div>
 @endsection
