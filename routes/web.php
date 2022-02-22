93d3f7b6a57c5d372d9af9218dc8612a5e55027f
Claudio Ferraro <claudio@justbetter.nl>
2022-02-22 11:09:30 +0100

    Add order to mollie-return

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::middleware('web')->group(function () {
-    Route::get('mollie-return/{orderHash}', function ($orderHash) {
+    Route::get('mollie-return/{orderHash}/{paymentToken}', function ($orderHash, $paymentToken) {
         $url = config('rapidez.magento_url').'/rest/'.config('rapidez.store_code').'/V1/mollie/get-order/'.$orderHash;
         $status = Http::get($url)->throw()[0]['status'];
 
@@ -9,6 +9,12 @@ Route::middleware('web')->group(function () {
             return redirect('cart');
         }
 
-        return view('mollie::'.$status);
+        $order = DB::table('mollie_payment_paymenttoken')
+            ->join('sales_order', 'sales_order.entity_id', '=', 'mollie_payment_paymenttoken.order_id')
+            ->join('sales_order_payment', 'sales_order_payment.parent_id', '=', 'sales_order.entity_id')
+            ->where('mollie_payment_paymenttoken.token', $paymentToken)
+            ->first();
+
+        return view('mollie::'.$status, ['order' => $order]);
     });
 });
